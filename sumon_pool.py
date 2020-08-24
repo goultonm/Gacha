9edd53b0aca55eef5bf102ad9d4375a2e480a33d
goultonm <goultonm@stu.otc.school.nz>
2020-08-24 12:27:02 +1200

    cool summon

--- sumon_pool.py
+++ sumon_pool.py
@@ -1,2 +1,12 @@
-servants = ["gillies_de_rais_caster" , "gillies_de_rais_saber"]
-craft_essence = ["Black keys" , "Before the awakening"]
+
+ssr = ["Artoria_Pendragon" , "Altera" , "Okita Sōji" , "Mordred" , "Nero (Bride)" , "Ryogi Shiki (Saber)" , "Miyamoto Musashi" , "Arthur Pendragon (Prototype)" , "Sigurd" , "Beni-enma" , "Dioscuri"]
+
+sr = ["Artoria Pendragon (Alter)" , "Nero" , "Siegfried","Chevalier d'Eon","Rama","Lancelot (Saber)","Gawain","Suzuka Gozen","Frankenstein (Saber)","Yagyu Munenori","Medb (Saber)","Diarmuid Ua Duibhne (Saber)","Lanling Wang","Lakshmibai"]
+
+r = ["Gaius Julius Caesar","Gilles de Rais (Saber)","Fergus mac Róich","Bedivere"]
+
+ssrce = ["ssrce crin ge","ssrce sad","ssrce josh","ssrce jett"]
+
+srce = ["srcebroDee","srcearniie","srcechirisu","srcebob",]
+
+rce = ["black keys", "white keys", "yorokobe", "shounen", "rejoice"]
