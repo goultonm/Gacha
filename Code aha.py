a4040ff5e76c2d31045071979a97062e7a97d24e
goultonm <goultonm@stu.otc.school.nz>
2020-11-07 00:45:56 +1300

    Final changes to the code, Added Comments and removed useless parts of teh code.

--- Code aha.py
+++ Code aha.py
@@ -2,32 +2,30 @@ from tkinter import *
 from sumon_pool import *
 import random
 from PIL import ImageTk, Image
-from FGOArt import *
 
+#########################################################
+#Imports the all librarys I need and my other code file
+#########################################################
 
-
-
-root = Tk()
-root.title ("Gacha Simulator") #the title things
-root.iconbitmap('cringe.ico') #makes Astolfo-kun the icon
-root.geometry('1600x900')
-canvas = Canvas(root, width = 1600, height = 900)
-background_file = PhotoImage(file = 'summon_background.png')
+root = Tk()  # The first step for the GUI #
+root.title ("Gacha Simulator")  #Sets the title of the top bar
+root.iconbitmap('cringe.ico')  #Sets the Image on the top bar
+root.geometry('1600x900')  #sets the size of the window
+canvas = Canvas(root, width = 1600, height = 900) #makes the UI background the same size as the window
+background_file = PhotoImage(file = 'summon_background.png') #sets background image
 background_cool = Label(image=background_file)
-background_cool.place(x=0, y=0, relwidth=1, relheight=1)
+background_cool.place(x=0, y=0, relwidth=1, relheight=1) #Finishes setting the background
 canvas.pack()
-root.resizable(0, 0)
+root.resizable(0, 0) #makes the window not resizable
 
 
 
-single_butt = ImageTk.PhotoImage(Image.open('summon_tile.png'))
-ten_butt = ImageTk.PhotoImage(Image.open('ten_summon_tile.png'))
-exit_butt = ImageTk.PhotoImage(Image.open('Close.png'))
+single_butt = ImageTk.PhotoImage(Image.open('summon_tile.png'))  #makes the summon button
+exit_butt = ImageTk.PhotoImage(Image.open('Close.png'))     #makes the exit button
 
 
-def roll():
-    percentage = random.randint(0,5)
-    print(percentage)
+def roll(): #uses a random number to generate what comes out of the gacha
+    percentage = random.randint(0,43) #number in range of 0 to 43
     if percentage ==0:
         summon_ssr()
     elif percentage <=3:
@@ -42,51 +40,38 @@ def roll():
         summon_rce()
 
 
-ssr_pool_size=len(servant.ssr_pool)
 
-
+ssr_pool_size=len(servant.ssr_pool)  #allows for the code to accept changing numbers of objects for future proofing
 def summon_ssr():
-    summoned_val = random.randint(0, ssr_pool_size-1)
+    summoned_val = random.randint(0, ssr_pool_size-1)   #picks object from class in sumon_pool
     summoned =  servant.ssr_pool[summoned_val]
-    print(summoned)
-    print(summoned.file)
-    load = Image.open(summoned.file)
+    load = Image.open(summoned.file)    #loads image stored in the object
     render = ImageTk.PhotoImage(load)
     img = Label(root, image=render, bd=0)
     img.image = render
-    img.place(x=800, y=350, anchor = CENTER)
+    img.place(x=800, y=350, anchor = CENTER)    #displays the image in the center of the GUI
 
-sr_pool_size=len(servant.sr_pool)
-
-
+sr_pool_size=len(servant.sr_pool)   #allows for the code to accept changing numbers of objects for future proofing
 def summon_sr():
-    summoned_val = random.randint(0, sr_pool_size-1)
+    summoned_val = random.randint(0, sr_pool_size-1)  #picks object from class in sumon_pool
     summoned =  servant.sr_pool[summoned_val]
-    print(summoned)
-    print(summoned.file)
-    load = Image.open(summoned.file)
+    load = Image.open(summoned.file)    #loads image stored in the object
     render = ImageTk.PhotoImage(load)
     img = Label(root, image=render, bd=0)
     img.image = render
-    img.place(x=800, y=350, anchor = CENTER)
+    img.place(x=800, y=350, anchor = CENTER)    #displays the image in the center of the GUI
 
-r_pool_size=len(servant.r_pool)
-
-
+r_pool_size=len(servant.r_pool)     #allows for the code to accept changing numbers of objects for future proofing
 def summon_r():
-    summoned_val = random.randint(0, r_pool_size-1)
+    summoned_val = random.randint(0, r_pool_size-1) #picks object from class in sumon_pool
     summoned =  servant.r_pool[summoned_val]
-    print(summoned)
-    print(summoned.file)
-    load = Image.open(summoned.file)
+    load = Image.open(summoned.file)    #loads image stored in the object
     render = ImageTk.PhotoImage(load)
     img = Label(root, image=render, bd=0)
     img.image = render
-    img.place(x=800, y=350, anchor = CENTER)
+    img.place(x=800, y=350, anchor = CENTER) #displays the image in the center of the GUI
 
-ssrce_pool_size=len(ssrce)
-
-
+ssrce_pool_size=len(ssrce)      #allows for the code to accept changing numbers of objects for future proofing
 def summon_ssrce():
     summoned_val = random.randint(0, ssrce_pool_size-1)
     summoned =  ssrce[summoned_val]
@@ -104,20 +89,13 @@ def summon_rce():
 
 
 def kys():
-    root.destroy()
-def nothing():
-    print("nothing")
-
+    root.destroy() #program ends itself
 
 close = Button(root, bd = '0', anchor = "ne", image = exit_butt, command = kys) # this is an exit button
 close.place(x = 1525, y =25)
 
-one_summon = Button(root, bd = '0', image = single_butt, command = roll, text = "1x Summon")  # made a button that rolls the gacha
-one_summon.place(x = 380, y =750)
-
-ten_summon = Button(root, bd = '0', anchor = 's', image = ten_butt, command = nothing, )  # made a button that does nothing
-ten_summon.place(x = 900, y =750)
-
+one_summon = Button(root, bd = '0', image = single_butt, command = roll, text = "1x Summon")  #button that rolls the gacha
+one_summon.place(x = 640, y =750 )
 
 
 root.mainloop()
--- sumon_pool.py
+++ sumon_pool.py
@@ -3,7 +3,7 @@ from enum import IntEnum
 import random
 
 
-class servant:
+class servant:  #creates class called servants for summoning
 
     class rarity (IntEnum):
         SSR = 1
@@ -13,21 +13,21 @@ class servant:
     R = rarity.R
     SR = rarity.SR
     SSR = rarity.SSR
-
-    names = ["Mafia Kajita" , "Mashu" , "Artoria Pendragon" , "Salter" , "Lily" , "Nero"]
-    rarity = [SSR,R,SSR,SR,SR,SR]
+    #names and rarity of the servants to create the objects
+    names = ["Mafia Kajita" , "Mashu" , "Artoria Pendragon" , "Salter" , "Lily" , "Nero" , "Siegfried", "Julius", "Altera", "Gilles", "Deon","Emiya", "Gilgamesh", "Robin Hood", "Nyarcher", "Euryle", "Arash", "Cuu", "Liz", "Benki","Proto Cu", "Leonidas", "Romulas", "Medusa", "George"]
+    rarity = [SSR,R,SSR,SR,SR,SR,SR,R,SSR,R,SR,SR,SSR,R,SR,R,R,R,SR,R,R,R,R,R,R]
 
     def __init__(self, name, rarity, num):
         self.name = name
         self.rarity = rarity
         self.num = num
-        if num < 10:
+        if num < 10: #creates file path so that the appropriate image can be found
             self.str_num = "00"+str(num)
         elif num < 100:
             self.str_num = "0"+str(num)
         else:
             self.str_num = str(num)
-        self.file = "FGOArt\servant_" + self.str_num + ".png"
+        self.file = "FGOArt\servant_" + self.str_num + ".png" #filename for the images
 
     def __repr__(self):  #unscrambles the jargon numbers
         return self.name
@@ -37,7 +37,7 @@ class servant:
     r_pool = []
 
 servants = []
-for i in range (6):
+for i in range (25): #turns the first 25 entries in the class into objects
     servants.append(servant(servant.names[i], servant.rarity[i], i))
 
 
@@ -51,20 +51,7 @@ for servant in servants:
     else:
         print("ErrOr")
 
-
-ssr = [
-    "Artoria Pendragon" , "Altera" , "Okita Sōji" , "Mordred" , "Nero (Bride)" , "Ryogi Shiki (Saber)" , "Miyamoto Musashi" , "Arthur Pendragon (Prototype)" , "Sigurd" , "Beni-enma" , "Dioscuri"
-    ]
-
-sr = [
-    "Artoria Pendragon (Alter)" , "Nero" , "Siegfried","Chevalier d'Eon","Rama","Lancelot (Saber)","Gawain","Suzuka Gozen","Frankenstein (Saber)","Yagyu Munenori","Medb (Saber)","Diarmuid Ua Duibhne (Saber)","Lanling Wang","Lakshmibai"
-    ]
-
-r = [
-    "Gaius Julius Caesar","Gilles de Rais (Saber)","Fergus mac Róich","Bedivere"
-    ]
-
-ssrce = ["ssrce crin ge","ssrce sad","ssrce josh","ssrce jett"]
+ssrce = ["ssrce crin ge","ssrce sad","ssrce josh","ssrce jett"]     #place holder variables to leave room for possible expansion
 
 srce = ["srcebroDee","srcearniie","srcechirisu","srcebob",]
 
